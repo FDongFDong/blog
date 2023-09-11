bb9e8ae31feaafd1e644322f2b99e4d6678a4358
FdongFdong <abnormal5626@naver.com>
2023-09-11 22:58:03 +0900

    feature: sitemap 적용 후 meta 태그까지 완료 db 수정해야함

--- pages/products.tsx
+++ pages/products.tsx
@@ -5,6 +5,7 @@ import Carousel from 'nuka-carousel'
 import Image from 'next/image'
 import { useState } from 'react'
 import React from 'react'
+import Head from 'next/head'
 const images = [
   {
     original: 'https://picsum.photos/id/1018/1000/600/',
@@ -49,6 +50,25 @@ export default function Products() {
   // return <ImageGallery items={images} />
   return (
     <>
+      <Head>
+        <meta
+          property="og:url"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="When Great Minds Don’t Think Alike"
+        />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
+        />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
+      </Head>
       <Carousel
         animation="zoom"
         autoplay
